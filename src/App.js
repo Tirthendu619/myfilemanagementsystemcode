cd7d0ae38e7f6c9857608d01ae0bb78bfae556cf
Tirthendu Sahu- <tirthendu619@gmail.com>
2020-03-24 09:14:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -101,10 +101,10 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Your Image</h1>
-              <p>This image is stored on IPFS & The Ethereum Blockchain!</p>
+              <h1>Your file</h1>
+              <p>This file is stored on IPFS & The Ethereum Blockchain!</p>
               <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
-              <h2>Upload Image</h2>
+              <h2>Upload file</h2>
               <form onSubmit={this.onSubmit} >
                 <input type='file' onChange={this.captureFile} />
                 <input type='submit' />
